30f6d92d50fc863b3377104755c072a2f5ec1c70
Tashus <alheadbme@gmail.com>
2019-01-04 23:27:05 -0600

    Create InputManager.

---
+++ InputManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+
+	string hAxis;
+	string vAxis;
+
+	Vector2 moveInput = new Vector2();
+
+
+	// Use this for initialization
+	void Start () {
+		hAxis = "Horizontal";
+		vAxis = "Vertical";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate () {
+		moveInput.Set(Input.GetAxis(hAxis), Input.GetAxis(vAxis));
+	}
+
+	public Vector2 GetMoveInput() {
+		return new Vector2(moveInput.x, moveInput.y);
+	}
+}
